a58f24a8c85efa8fa6341f0b31d4a45d7d8ace8a
Eugene Eroshkin <jekis01@gmail.com>
2016-09-27 23:04:14 +0700

    Extract news from html and create objects.

--- popup/news.js
+++ popup/news.js
@@ -13,18 +13,69 @@ function hidePopup(delay) {
     }, delay);
 }
 
+function extractNews(html) {
+    var regExps = {
+        allNews: /<p[^>]+class="news-item"(?:.|[\r\n])+?<\/p>/gmi,
+        pictureUrl: /src="(.+?)"/i,
+        newsTitle: /alt="(.+?)"/i,
+        newsPublishedDate: /news-date-time[^>]+>(.+?)<\/span/i,
+        newsUrl: /href="(.+?news=\d+)"/i
+    };
+
+    var newsHtml = html.match(regExps.allNews);
+
+    if (!newsHtml) {
+        return showMessage('Новости не были найдены на странице.');
+    }
+
+    var news = [];
+
+    for (var i = 0; i < newsHtml.length; i++) {
+        var matches;
+        var newsItem = {
+            title: null,
+            publishedAt: null,
+            url: null,
+            pictureUrl: null
+        };
+
+        // Extract title
+        matches = newsHtml[i].match(regExps.newsTitle);
+        if (matches) {
+            newsItem.title = matches[1];
+        }
+
+        // Extract date
+        matches = newsHtml[i].match(regExps.newsPublishedDate);
+        if (matches) {
+            newsItem.publishedAt = matches[1];
+        }
+
+        // Extract picture
+        matches = newsHtml[i].match(regExps.pictureUrl);
+        if (matches) {
+            newsItem.pictureUrl = host + matches[1];
+        }
+
+        // Extract url
+        matches = newsHtml[i].match(regExps.newsUrl);
+        if (matches) {
+            newsItem.url = host + matches[1];
+        }
+
+        news.push(newsItem);
+    }
+
+    return news;
+}
+
 function renderNewsTo(responseText, newsContainer) {
     newsContainer.empty();
 
-    var newsRegExp = /<span[^>]*news-date-time"[^>]*>([^<]+)<\/span>[^a]+<a[^<]+<[^<]+<[^<]+<\/a>/gmi;
-    var imagesRegExp = /preview_picture[^>]+src=".+?"/gmi;
-    var dateRegExp = />((\d+)\s[^\d]+)\s\d+</i; // Matches ">((23) Сентября) 2016<"
-    var imageSrcRegExp = /src="(.+?)"/;
-    var news = responseText.match(newsRegExp);
-    var images = responseText.match(imagesRegExp);
+    var news = extractNews(responseText);
     var prevDate;
     var today = new Date();
-    var noNewsForToday = news[0].match(dateRegExp)[2] != today.getDate();
+    var noNewsForToday = news[0].publishedAt.match(/^\d+/) != today.getDate();
     var todayText = 'Сегодня';
     var noNewsText = 'Нет новостей';
 
@@ -32,34 +83,38 @@ function renderNewsTo(responseText, newsContainer) {
         newsContainer.append($('<h4>' + todayText + '</h4><div>'+ noNewsText +'</div>'));
     }
 
-    $.each(news, function (i, newsItem) {
-        var date = newsItem.match(dateRegExp);
+    for (var i = 0; i < news.length; i++) {
+        var date = news[i].publishedAt.match(/^(\d+) [^\s]+/i); // Matches "24 сентября"
         var showDate;
 
         // Group news by date
-        if (date[1] !== prevDate) {
-            showDate = date[2] == today.getDate() ? todayText : date[1];
+        if (date[0] !== prevDate) {
+            showDate = date[1] == today.getDate() ? todayText : date[0];
             newsContainer.append($('<h4>' + showDate + '</h4>'));
-            prevDate = date[1];
+            prevDate = date[0];
         }
 
         // Create div with background image
         var image = '';
-        if (images && images[i] && images[i].match(imageSrcRegExp)) {
-            var src = host + images[i].match(imageSrcRegExp)[1];
-            image = '<div class="image" style="background-image: url('+ src +');"></div> ';
+        if (news[i].pictureUrl) {
+            image = '<div class="image" style="background-image: url('+ news[i].pictureUrl +');"></div> ';
         }
 
-        newsContainer.append($('<div>' + image + newsItem + '</div>'));
-    });
+        newsContainer.append($(
+            '<div>' +
+                image +
+                '<span class="news-date-time">'+ news[i].publishedAt +'</span><br>' +
+                (
+                    news[i].url ?
+                    '<a class="title" href="'+ news[i].url +'">' + news[i].title + '</a>' :
+                    '<span class="title">'+  news[i].title +'</span>'
+                ) +
+            '</div>'
+        ));
+    }
 
     newsContainer.find('a').each(function () {
-        var $this = $(this);
-        var href = $this.attr('href');
-        $this
-            .attr('href', host + href)
-            .attr('target', '_blank')
-        ;
+        $(this).attr('target', '_blank');
     });
 }
 
